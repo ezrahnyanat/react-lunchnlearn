d7fd9b6effb075ffc34b768c7d2ec29ceea5215f
Ezrah Nyanat <ezrah.nyanat@life.church>
2019-01-27 18:18:39 -0600

    Updates

---
+++ precache-manifest.a920650d0781fd25f208878820454b32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3bc9f602ee64c6cde53",
+    "url": "/react-lunchnlearn/static/js/runtime~main.f3bc9f60.js"
+  },
+  {
+    "revision": "ece3ae0976e74e10b8df",
+    "url": "/react-lunchnlearn/static/js/main.ece3ae09.chunk.js"
+  },
+  {
+    "revision": "11dac3cdf202915bf55a",
+    "url": "/react-lunchnlearn/static/js/1.11dac3cd.chunk.js"
+  },
+  {
+    "revision": "ece3ae0976e74e10b8df",
+    "url": "/react-lunchnlearn/static/css/main.d4d9fb8a.chunk.css"
+  },
+  {
+    "revision": "a6be5cb0dbb74b1eb5544196da2d5d5a",
+    "url": "/react-lunchnlearn/index.html"
+  }
+];
